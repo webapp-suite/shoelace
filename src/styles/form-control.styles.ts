a3aea53b88fc5a9a089337de3e7459c22fc24ec2
Kimi Gao <kimi.gao@outlook.com>
2021-11-18 01:35:20 +0800

    remove changes for form-control__label

--- src/styles/form-control.styles.ts
+++ src/styles/form-control.styles.ts
@@ -14,8 +14,6 @@ export default css`
     display: inline-block;
     color: var(--sl-input-label-color);
     margin-bottom: var(--sl-spacing-3x-small);
-    font-weight: var(--sl-font-weight-semibold);
-    text-transform: uppercase;
   }
 
   .form-control--has-label.form-control--small .form-control__label {
